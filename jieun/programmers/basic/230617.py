d313a01eada1b08730c195485d7b80aaef55242f
zinnie1025 <luckyjj1025@gmail.com>
2023-06-18 09:52:24 +0900

    programmers

--- jieun/programmers/basic/230617.py
+++ jieun/programmers/basic/230617.py
@@ -73,42 +73,6 @@ def solution(n):
                 answer += (i * i)
     return answer
 
-#_ 프로세스
-
-priorities = [2, 1, 3, 2]
-location = 2
-
-#* priorities 숫자가 클수록 우선순위가 높다.
-
-#* process = [A, B, C, D]
-#* location (인덱스를 의미)
-
-#* 1. 큐에서 A를 꺼낸다 -> 우선순위를 체크 -> C의 우선순위가 더 높음 -> A를 큐에 맨 뒤로 집어넣음. 큐: [B, C, D, A]
-#* 2. 큐에서 B를 꺼낸다 -> 우선순위를 체크 -> C의 우선순위가 더 높음 -> B를 큐에 맨 뒤로 집어넣음. 큐: [C, D, A, B]
-#* 3. 큐에서 C를 꺼낸다 -> 우선순위를 체크 -> C를 그대로 실행. 큐: [D, A, B], 실행: [C]
-#* 4. 큐에서 D를 꺼낸다 -> 우선순위를 체크 -> (D도 우선순위가 2고, A도 2이므로) D를 그대로 실행. 큐: [A, B], 실행: [C, D]
-#* 5. 큐에서 A를 꺼낸다 -> 우선순위를 체크 -> (A는 우선순위가 2고, B는 1이므로) A를 그대로 실행. 큐: [B], 실행: [C, D, A]
-#* 6. 큐에서 B를 꺼낸다 -> 우선순위를 체크 -> B를 그대로 실행. 큐: [], 실행: [C, D, A, B]
-
-def solution(priorities, location):
-    #* 위치와 우선순위를 저장한 큐를 생성
-    myQueue = [(idx, priorities) for idx, priorities in enumerate(priorities)]
-    #* answer : 몇 번째 실행인지 저장하는 변수
-    answer = 0
-    while True:
-        #* 큐에서 맨 앞에 요소를 빼내고
-        current = myQueue.pop(0)
-        
-        #* any : 안에 조건 중 하나라도 있으면,
-        #* myQueue 안에 (인덱스, 우선순위) 튜플 중 우선순위 값들을 비교
-        if any(current[1] < q[1] for q in myQueue):
-            myQueue.append(current)
-        else:
-            #* current 현재 우선순위가 제일 높은 상황
-            #* answer : 몇 번째 실행인지 저장하는 변수
-            answer += 1
-            if current[0] == location:
-                return answer
 
 
 
