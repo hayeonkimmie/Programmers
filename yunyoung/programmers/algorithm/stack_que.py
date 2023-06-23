b99427dee91c8b28e33d028abf4bde0c1688fc4d
JyunDev <jyunxxxxx@gmail.com>
2023-06-23 15:57:47 +0900

    프로그래머스 230623

--- yunyoung/programmers/algorithm/stack_que.py
+++ yunyoung/programmers/algorithm/stack_que.py
@@ -30,7 +30,6 @@ class Stack:
     
 #TODO Stack 이용 
 #TODO 효율성 하락 
-
 stack = Stack()
 def solution(arr):
     for num in arr:
@@ -48,4 +47,39 @@ def solution(arr):
     return answer
 
 
+#_ 기능개발 
+#TODO 
+#progresses  	            speeds	             return
+#[93, 30, 55]	            [1, 30, 5]           [2, 1]
+#[95, 90, 99, 99, 80, 99]	[1, 1, 1, 1, 1, 1]	 [1, 3, 2]
 
+import math 
+
+def solution(progresses, speeds):
+    answer = [1]
+    day = []
+    rest = 1
+    
+    for i, p in enumerate(progresses):
+        day.append(math.ceil((100 - p) / speeds[i]))
+        
+    for i, d in enumerate(day):
+        rest = 1
+        if sum(answer) == len(day):
+            break
+        if len(day) > (i + 1) and d > day[i + 1] :
+            rest += 1
+            continue
+        else:
+            rest = 1
+
+        answer.append(rest)
+    
+    print(day)
+    print(answer)
+
+
+p = [95, 90, 99, 99, 80, 99]
+s = [1, 1, 1, 1, 1, 1]
+
+solution(p,s)
